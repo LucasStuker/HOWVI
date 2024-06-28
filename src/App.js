637bcfb707568ef618f6ea6f3051641d87c75385
Lucas <lucasstuker@gmail.com>
2024-06-28 19:02:21 -0300

    arrumado

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Mural from './components/Mural';
 import Header from './components/header/header';
 import Aluno from './components/Alunos';
 import Disciplinas from './components/Disciplinas';
-import NovoRegistro from './components/NovoRegistro';
 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
@@ -15,7 +14,6 @@ function App() {
         <Routes>
           <Route path='/alunos' element={<Aluno />} />
           <Route path='/disciplinas' element={<Disciplinas />} />
-          <Route path='/novoregistro' element={<NovoRegistro />} />
           <Route path='/' element={<Mural />} />
         </Routes>
       </div>
